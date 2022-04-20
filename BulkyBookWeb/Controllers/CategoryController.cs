4750a5980c6266b52d17ef8cb7185eb27f89d489
Lejla1502 <lejlasphotmail.com>
2022-04-20 15:33:27 +0200

    create category validations

---
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,41 @@
+﻿using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _ctx;
+        public CategoryController(ApplicationDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> cat = _ctx.Categories;
+
+            return View(cat);
+        }
+
+        public IActionResult Create() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Save(Category cat)
+        {
+            if (ModelState.IsValid)
+            {
+                _ctx.Categories.Add(cat);
+                _ctx.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            else
+                return View("Create",cat);
+        }
+    }
+}
---
+++ BulkyBookWeb/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookWeb.Models
+{
+    public class Category
+    {
+        [Key]  //this makes sure Id is primary key and is identity column
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+    }
+}
