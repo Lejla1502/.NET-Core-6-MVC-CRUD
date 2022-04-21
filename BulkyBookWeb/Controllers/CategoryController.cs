70ef05d89333342f2f8c6ed5702cc6813683ef75
Lejla1502 <lejlasphotmail.com>
2022-04-21 12:55:12 +0200

    CRUD for category done

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -37,11 +37,70 @@ namespace BulkyBookWeb.Controllers
             {
                 _ctx.Categories.Add(cat);
                 _ctx.SaveChanges();
-
+                TempData["success"] = "Category created successfully";
                 return RedirectToAction("Index");
             }
             else
                 return View("Create",cat);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null | id == 0)
+            {
+                return NotFound();   
+            }
+            Category cat = _ctx.Categories.Find(id);
+            //category cat = _ctx.Categories.FirstOrDefault(u=>u.Id==id);
+            //Category cat = _ctx.Categories.SingleOrDefault(u => u.Id == id);
+
+            if (cat == null)
+                return NotFound();
+
+            return View(cat);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult SaveAfterEdit(Category cat)
+        {
+
+            if (cat.Name == cat.DisplayOrder.ToString())
+            {
+                //we're putting name as key beacuse we want to display error
+                //for input field "Name" of category
+                ModelState.AddModelError("name", "Name and order cannot be the same");
+            }
+            if (ModelState.IsValid)
+            {
+                _ctx.Categories.Update(cat);
+                _ctx.SaveChanges();
+                TempData["success"] = "Category edited successfully";
+                return RedirectToAction("Index");
+            }
+            else
+                return View("Edit", cat);
+        }
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        public IActionResult Delete(int? id)
+        {
+
+            if (id == null | id == 0)
+            {
+                return NotFound();
+            }
+
+            Category obj = _ctx.Categories.Find(id);
+
+            if (obj == null)
+                return NotFound();
+
+            _ctx.Categories.Remove(_ctx.Categories.Find(id));
+            _ctx.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
+            return RedirectToAction("Index");
         }
     }
 }
--- BulkyBookWeb/Models/Category.cs
+++ BulkyBookWeb/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BulkyBookWeb.Models
 {
@@ -7,7 +8,10 @@ namespace BulkyBookWeb.Models
         [Key]  //this makes sure Id is primary key and is identity column
         public int Id { get; set; }
         [Required]
+        [RegularExpression(@"^[a-zA-Z]+[ a-zA-Z-_]*$", ErrorMessage = "Only use letters")]
         public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1,100, ErrorMessage ="Display Order must be between 1 and 100 only!")]
         public int DisplayOrder { get; set; }
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
     }
