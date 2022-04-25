53a1f47f82e647c9bdf3fef7e5ecde0824460995
Lejla1502 <lejlasp@hotmail.com>
2022-04-25 04:33:35 +0200

    adding create cover type

--- BulkyBookWeb/Areas/Admin/Controllers/CoverTypeController.cs
+++ BulkyBookWeb/Areas/Admin/Controllers/CoverTypeController.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyBookWeb.Admin.Controllers
 {
+    [Area("Admin")]
+
     public class CoverTypeController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
@@ -16,6 +18,27 @@ namespace BulkyBookWeb.Admin.Controllers
             IEnumerable<CoverType> cat = _unitOfWork.CoverType.GetAll();
 
             return View(cat);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Save(CoverType obj)
+        {
+           
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.CoverType.Add(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Category created successfully";
+                return RedirectToAction("Index");
+            }
+            else
+                return View("Create", obj);
         }
     }
 }
