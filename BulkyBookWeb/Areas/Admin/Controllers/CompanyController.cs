ef4a902db039538acc299d67f02e92668727c73e
Lejla1502 <lejlasphotmail.com>
2022-04-29 15:42:05 +0200

    finished Upsert

--- BulkyBookWeb/Areas/Admin/Controllers/CompanyController.cs
+++ BulkyBookWeb/Areas/Admin/Controllers/CompanyController.cs
@@ -19,6 +19,55 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
             return View();
         }
 
+        public IActionResult Upsert(int? id)
+        {
+            Company obj = new Company();
+            if (id == 0 | id == null)
+            {
+                //ViewBag.CategoryList = CategoryList;
+                ////same thing as above, just different approach
+                //ViewData["CoverTypeList"] = CoverTypeList;
+
+                return View(obj);
+            }
+            else
+            {
+                obj = _unitOfWork.Company.GetFirstOrDefault(x => x.Id == id);
+                if (obj == null)
+                    return NotFound();
+
+                return View(obj);
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Company obj)
+        {
+            if (ModelState.IsValid)
+            {
+               
+                if (obj.Id == 0)
+                {
+                    _unitOfWork.Company.Add(obj);
+                    _unitOfWork.Save();
+                    TempData["success"] = "Product created successfully";
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    _unitOfWork.Company.Update(obj);
+                    _unitOfWork.Save();
+                    TempData["success"] = "Product edited successfully";
+                    return RedirectToAction("Index");
+                }
+
+            }
+            return View(obj);
+        }
+
+
+
 
         #region API CALLS
         [HttpGet]
