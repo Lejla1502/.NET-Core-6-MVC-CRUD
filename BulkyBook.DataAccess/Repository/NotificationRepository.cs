0a66d59b6b36fa9ae52ad2db00a34ba93a61fbbb
Lejla1502 <lejlasp@hotmail.com>
2022-05-15 21:36:36 +0200

    adding notification repository

---
+++ BulkyBook.DataAccess/Repository/NotificationRepository.cs
@@ -0,0 +1,30 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class NotificationRepository:Repository<Notification>, INotificationRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public NotificationRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Notification notification)
+        {
+            _db.Notifications.Update(notification);
+        }
+
+        public void ReadNotification(int id)
+        {
+            var notificationFromDb = _db.Notifications.FirstOrDefault(x => x.Id == id);
+            notificationFromDb.IsRead = true;
+        }
+    }
+}
