0f11e46ff2553889c66fbc3af7223844d4499789
Lejla1502 <lejlasphotmail.com>
2022-05-17 14:41:20 +0200

    creating application user controller to display all users for admin

---
+++ BulkyBookWeb/Areas/Admin/Controllers/ApplicationUserController.cs
@@ -0,0 +1,26 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using BulkyBook.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = StaticDetails.Role_Admin)]
+
+    public class ApplicationUserController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ApplicationUserController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+         public IActionResult Index()
+        {
+            IEnumerable<ApplicationUser> users = _unitOfWork.ApplicationUser.GetAll();
+            return View(users);
+        }
+    }
+}
