6d32f22bea2c731e131a51caff511a14ea8a03cc
Lejla1502 <lejlasphotmail.com>
2022-05-19 11:00:56 +0200

    returning user profile to its semi-original form

--- BulkyBookWeb/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ BulkyBookWeb/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -35,6 +35,7 @@ namespace BulkyBookWeb.Areas.Identity.Pages.Account.Manage
         /// </summary>
         public string Username { get; set; }
 
+
         /// <summary>
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
@@ -181,7 +182,6 @@ namespace BulkyBookWeb.Areas.Identity.Pages.Account.Manage
                 return LocalRedirect(url);
             }
            
-            
         }
     }
 }
