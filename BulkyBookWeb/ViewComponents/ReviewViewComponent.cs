d1fea673a717b3b69496161a59bf36c5862ac260
Lejla1502 <lejlasphotmail.com>
2022-06-14 15:34:51 +0200

    rating system half way through

--- BulkyBookWeb/ViewComponents/ReviewViewComponent.cs
+++ BulkyBookWeb/ViewComponents/ReviewViewComponent.cs
@@ -39,12 +39,12 @@ namespace BulkyBookWeb.ViewComponents
             //var o = _db.Reviews.ToList().SelectMany(reviews1=>reviews1.ApplicationUser,
             //    (reviews, users)=> new { review, user});
             
-            var some1= _db.ApplicationUsers.GroupJoin(_db.Reviews,
-                user => user.Id,
-                review => review.ApplicationUserId,
-                (x, y) => new { User = x, Review = y }
-                ).SelectMany(x=>x.Review.DefaultIfEmpty(),
-                (x,y)=> new { usr=x.User, Review=y });
+            //var some1= _db.ApplicationUsers.GroupJoin(_db.Reviews,
+            //    user => user.Id,
+            //    review => review.ApplicationUserId,
+            //    (x, y) => new { User = x, Review = y }
+            //    ).SelectMany(x=>x.Review.DefaultIfEmpty(),
+            //    (x,y)=> new { usr=x.User, Review=y });
 
             //reviewVM.Reviews =_db.ApplicationUsers.GroupJoin(_db.Reviews,
             //    user=>user.Id,
@@ -54,21 +54,24 @@ namespace BulkyBookWeb.ViewComponents
             //Reviews.Include(y => y.ApplicationUser).Where(x => x.ProductId == bookID);
             reviewVM.Reviews = _unitOfWork.Review.GetAll(r => r.ProductId == bookID, includeProperties: "ApplicationUser");
             reviewVM.Title = _unitOfWork.Product.GetFirstOrDefault(p => p.Id == bookID).Title;
-         
-            int sum = 0;
-            foreach(var r in reviewVM.Reviews)
+
+            if (reviewVM.Reviews.Count() > 0)
             {
-                sum += r.Rating;
+                int sum = 0;
+                foreach (var r in reviewVM.Reviews)
+                {
+                    sum += r.Rating;
+                }
+
+                int avg = (int)(sum / reviewVM.Reviews.Count());
+
+                reviewVM.SumOfRatings = sum;
+                reviewVM.NumOfRatings = reviewVM.Reviews.Count();
+                reviewVM.AvgRating = avg;
             }
-
-            int avg = (int)(sum / reviewVM.Reviews.Count());
-
-            reviewVM.SumOfRatings = sum;
-            reviewVM.NumOfRatings = reviewVM.Reviews.Count();
-            reviewVM.AvgRating = avg;
-
             return View("", reviewVM);
-            
+           
+           
         }
     }
 }
