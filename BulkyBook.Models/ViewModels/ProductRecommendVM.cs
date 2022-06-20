15658ca319785e0a2ebadbef51f56dc83703fd91
Lejla1502 <lejlasp@hotmail.com>
2022-06-20 15:17:38 +0200

    solved display of rating for recommended products

---
+++ BulkyBook.Models/ViewModels/ProductRecommendVM.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.Models.ViewModels
+{
+    public class ProductRecommendVM
+    {
+        public ProductInfo[] Products { get; set; }
+
+        public class ProductInfo
+        {
+            public int Id { get; set; }
+            public string Title { get; set; }
+            public string Author { get; set; } //special pricing if a person is buying 50 books
+            public string ImageUrl { get; set; }
+            public double Price { get; set; }
+            public int AvgRating { get; set; }
+        }
+    }
+}
