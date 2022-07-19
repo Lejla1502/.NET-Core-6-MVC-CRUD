8a49e222445f63e28503f7797f05b5df531673d2
Lejla1502 <lejlasp@hotmail.com>
2022-07-19 07:48:09 +0200

    adding author view model

---
+++ BulkyBook.Models/ViewModels/AuthorVM.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.Models.ViewModels
+{
+    public class AuthorVM
+    {
+        public IEnumerable<AuthorInfo> Authors { get; set; }
+
+        public class AuthorInfo
+        {
+            public int Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string NumOfBooks { get; set; }
+
+        }
+    }
+}
