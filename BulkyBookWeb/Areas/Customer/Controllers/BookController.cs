73dfa32458a2acd0198818dc3c16366224b129ca
Lejla1502 <lejlasp@hotmail.com>
2022-07-21 20:15:08 +0200

    book controller added

---
+++ BulkyBookWeb/Areas/Customer/Controllers/BookController.cs
@@ -0,0 +1,22 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class BookController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        //private readonly ILogger<HomeController> _logger;
+
+        public BookController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
