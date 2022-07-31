825371ee09eca274c168e1c6409ef0d1b7791c15
Lejla1502 <lejlasp@hotmail.com>
2022-08-01 00:15:56 +0200

    edit category repository

--- BulkyBook.DataAccess/Repository/CategoryRepository.cs
+++ BulkyBook.DataAccess/Repository/CategoryRepository.cs
@@ -1,5 +1,6 @@
 ﻿using BulkyBook.DataAccess.Repository.IRepository;
 using BulkyBook.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,6 +21,32 @@ namespace BulkyBook.DataAccess.Repository
         public void Update(Category category)
         {
             _db.Categories.Update(category);
+        }
+
+        public List<Category> GetPopularCategories()
+        {
+            var something = _db.OrderDetails.GroupBy(x => x.ProductId).Select(s => new { Bestseller_PrdouctID = s.Key, Count = s.Count() }).OrderByDescending(y => y.Count);
+
+            List<Product> bestsellers = new();
+            List<Category> popularCategories = new();
+
+            foreach (var p in something)
+            {
+                bestsellers.Add((Product)_db.Products.Include(p => p.Category).Where(x => x.Id == p.Bestseller_PrdouctID));
+            }
+
+            if (bestsellers.Count() < 1)
+            {
+                return null;
+            }
+
+            foreach(var c in bestsellers)
+            {
+                popularCategories.Add((Category)_db.Categories.Where(x => x.Id == c.CategoryId));
+            }
+            
+
+            return popularCategories;
         }
     }
 }
