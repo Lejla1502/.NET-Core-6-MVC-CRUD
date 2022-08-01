8ccd3f2076d45b0717e3c48865540136f6ba0289
Lejla1502 <lejlasp@hotmail.com>
2022-08-01 22:35:19 +0200

    section for new arrivals added

--- BulkyBookWeb/Areas/Customer/Controllers/HomeController.cs
+++ BulkyBookWeb/Areas/Customer/Controllers/HomeController.cs
@@ -125,6 +125,7 @@ namespace BulkyBookWeb.Customer.Controllers
 
             productReviewVM.Bestsellers = _unitOfWork.Product.GetBestsellers();
             productReviewVM.PopularCategories = _unitOfWork.Category.GetFourPopularCategories();
+            productReviewVM.NewBooks = _unitOfWork.Product.GetAll().OrderByDescending(x => x.CreatedAt).Take(4).ToList();
 
            // ProductHomePageVM products= new ProductHomePageVM
 
