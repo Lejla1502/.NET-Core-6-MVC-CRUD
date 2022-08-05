bb5dcfc81b9cd7654dc1c056c5344013c41f648b
Lejla1502 <lejlasphotmail.com>
2022-08-05 11:45:50 +0200

    fixed UI for book index

--- BulkyBookWeb/Program.cs
+++ BulkyBookWeb/Program.cs
@@ -89,12 +89,12 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-    app.UseSwagger();
-    app.UseSwaggerUI(options =>
-    {
-        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-        options.RoutePrefix = string.Empty;
-    });
+    //app.UseSwagger();
+    //app.UseSwaggerUI(options =>
+    //{
+    //    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    //    options.RoutePrefix = string.Empty;
+    //});
 }
 
 //here we enable swagger
