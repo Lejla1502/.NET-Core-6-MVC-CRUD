ad2cf5b9b2fe29fcaa275d5da3fe38f9942b2ae5
Lejla1502 <lejlasphotmail.com>
2022-08-17 15:38:38 +0200

    need to remove from localstorage on save

--- BulkyBook.Models/Product.cs
+++ BulkyBook.Models/Product.cs
@@ -14,6 +14,7 @@ namespace BulkyBook.Models
         public int Id { get; set; }
         [Required]
         public string Title { get; set; }
+        [Required]
         public string Description { get; set; }
         [Required]
         public string ISBN { get; set; }
